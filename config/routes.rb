d93d22f4f89fe6d9241e42693a3f77bcc182ec6f
JacobYun <jacob.yun429@gmail.com>
2017-09-09 16:46:29 -0700

    completed rails: static pages checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  root 'welcome#index' #makes default page be the welcome index view
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
