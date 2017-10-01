333a922e030710b6e413c2a7510fa8c7f18a1706
JacobYun <jacob.yun429@gmail.com>
2017-09-30 17:20:22 -0700

    completed assignment for rails :intro to CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@ class PostsController < ApplicationController
   def index
 #declare instance variable @posts and assign it a collection of Post objects using all
     @posts = Post.all
+#assignment for first and every fifth post after it with text SPAM
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
