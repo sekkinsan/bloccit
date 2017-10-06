f4375b717c614ab39b398badac0997f90bc270bf
JacobYun <jacob.yun429@gmail.com>
2017-10-06 14:55:49 -0700

    completed curriculum for Rails: MORE CRUD checkpoint

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -82,14 +82,65 @@ let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData
     end
   end
 
-#comment out tests for show, new, and edit until we write implementation later
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, params: { id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
 
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it "renders the #edit view" do
+      get :edit, params: { id: my_post.id }
+  #expect edit view to render when a post is edited
+      expect(response).to render_template :edit
+    end
 
+  #test that edit assigns the correct post to be updated to @post
+    it "assigns post to be updated to @post" do
+      get :edit, params: { id: my_post.id }
+
+      post_instance = assigns(:post)
+
+      expect(post_instance.id).to eq my_post.id
+      expect(post_instance.title).to eq my_post.title
+      expect(post_instance.body).to eq my_post.body
+    end
+  end
+
+    describe "PUT update" do
+      it "updates post with expected attributes" do
+        new_title = RandomData.random_sentence
+        new_body = RandomData.random_paragraph
+
+        put :update, params: { id: my_post.id, post: {title: new_title, body: new_body } }
+#test that @post was updated with the title and body passed to update and that @post's id was not changed.
+        updated_post = assigns(:post)
+        expect(updated_post.id).to eq my_post.id
+        expect(updated_post.title).to eq new_title
+        expect(updated_post.body).to eq new_body
+      end
+#expect to be redirected to the posts show view after the update.
+      it "redirects to the updated post" do
+        new_title = RandomData.random_sentence
+        new_body = RandomData.random_paragraph
+
+        put :update, params: { id: my_post.id, post: {title: new_title, body: new_body } }
+        expect(response).to redirect_to my_post
+      end
+    end
+
+    describe "DELETE destroy" do
+      it "deletes the post" do
+        delete :destroy, params: { id: my_post.id }
+  #search database for post with id my_post.id, test asserts database won't have matching post after destroy is called.
+        count = Post.where({id: my_post.id}).size
+        expect(count).to eq 0
+      end
+
+      it "redirects to posts index" do
+        delete :destroy, params: { id: my_post.id }
+  #expect to be redirected to posts page
+        expect(response).to redirect_to posts_path
+      end
+    end
 
 end
