285d85d853349a777f2176b0aaa161134dee471e
JacobYun <jacob.yun429@gmail.com>
2017-10-22 20:01:58 -0700

    completed authentication - signing up checkpoint

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+    let(:new_user_attributes) do
+        {
+            name: "Blochead",
+            email: "blochead@bloc.io",
+            password: "blochead",
+            password_confirmation: "blochead"
+        }
+    end
+
+
+    describe "GET new" do
+        it "returns http success" do
+            get :new
+            expect(response).to have_http_status(:success)
+        end
+
+        it "instantiates a new user" do
+            get :new
+            expect(assigns(:user)).to_not be_nil
+        end
+    end
+
+    describe "POST create" do
+        it "returns an http redirect" do
+            post :create, params: { user: new_user_attributes }
+            expect(response).to have_http_status(:redirect)
+        end
+
+    #test database count on the users table increases by one when we issue a POST to create
+        it "creates a new user" do
+            expect{
+                post :create, params: { user: new_user_attributes }
+        }.to change(User, :count).by(1)
+        end
+    
+    #test that we set user.name properly when creating a user
+        it "sets user name properly" do
+            post :create, params: { user: new_user_attributes }
+            expect(assigns(:user).name).to eq new_user_attributes[:name]
+        end
+    
+    #test that we set user.email properly when creating a user
+        it "sets user email properly" do
+            post :create, params: { user: new_user_attributes }
+            expect(assigns(:user).email).to eq new_user_attributes[:email]
+        end   
+
+    #test taht we set user.password properly when creating a user
+        it "sets user password properly" do
+            post :create, params: { user: new_user_attributes }
+            expect(assigns(:user).password).to eq new_user_attributes[:password]
+        end   
+
+    #test that we set user.password_confirmation properly when creating a user
+        it "sets user name properly" do
+            post :create, params: { user: new_user_attributes }
+            expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+        end   
+    end
+    
+end
