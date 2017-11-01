f2ffa30939598d735b95e85345db257ddcf3067d
JacobYun <jacob.yun429@gmail.com>
2017-10-31 20:36:12 -0700

    completed assignment and fixed rspec issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
     
-    include SessionsHelper
-
     def form_group_tag(errors, &block)
         css_class = 'form-group'
         css_class << ' has-error ' if errors.any?
@@ -13,5 +11,5 @@ module ApplicationHelper
         gravatar_id = Digest::MD5::hexdigest(user.email).downcase
         "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
     end
-    
+
 end
